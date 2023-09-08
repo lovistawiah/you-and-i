c1ff3b3aa1dfdc269ecf647db5ff8af168415afb
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-09-08 20:13:01 +0000

    fixed message panel

--- public/src/components/MessagePanel.jsx
+++ public/src/components/MessagePanel.jsx
@@ -1,6 +1,241 @@
+// import { Helmet } from 'react-helmet'
+import '../styles/message-panel.css'
+import dpSvg from '../svg/dp.svg'
+import sendSvg from '../svg/send-button.svg'
 const MessagePanel = () => {
+
   return (
-    <div>MessagePanel</div>
+    <div className='message-container'>
+      <section className="user">
+        <section className="dp-name-status">
+          <section className="dp-holder">
+            <img src={dpSvg} alt="dp of user" className='dp' />
+          </section>
+          <section className="username-status">
+            <section className="username">Lovis Tawiah</section>
+            <section className="status">online</section>
+          </section>
+        </section>
+      </section>
+      <section className="messages">
+         
+        <section className="date">
+          <section className="message-date">12/03/2023</section>
+        </section>
+
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        <section className="message-holder">
+          <section className="message-content-holder">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+
+        <section className="message-holder sender">
+          <section className="message-content-holder sender-content">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+  
+        <section className="message-holder sender">
+          <section className="message-content-holder sender-content">
+            hello, it is me and I’m typing and it is working as expected and what do you think
+          </section>
+          <section className="message-status">Delivered</section>
+        </section>
+        
+
+      </section>
+      <form className="send-message"
+      >
+        <textarea
+          onKeyUp={(e) => {
+            e.target.style.height = '36px'
+            let height = e.target.scrollHeight 
+            height +=2
+            e.target.style.height = `${height}px`
+          }}
+          className='message-input' placeholder='enter message here...'></textarea>
+
+        <button className='input-button'>
+          <img src={sendSvg} alt="" className='send-svg' />
+        </button>
+      </form>
+    </div>
   )
 }
 export default MessagePanel
