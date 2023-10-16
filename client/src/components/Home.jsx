4aef4dfaed7a52371f711131ce9bb2f9c43190b0
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-10-16 14:26:41 +0000

    move the messages wrapper into a new file called Messages.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -69,45 +69,6 @@ const Home = () => {
                 </section>
             </section>
 
-            <section className="messages-panel">
-                <section className="chat-info">
-                    <section className="chat-dp">
-                        <img src={Dp} alt="user db" />
-                    </section>
-                    <section className="chat-username-status">
-                        <section className="chat-username">Lovis Tawiah</section>
-                        <section className="chat-status">Online</section>
-                    </section>
-                </section>
-                <section className="messages">
-                    <section className="date-box">
-                        <section className="messages-date">
-                            12/03/2023
-                        </section>
-                    </section>
-                    <section className="message">
-                        <section className="message-content">hello, it is me and I’m typing and it is working as expected and what do you think</section>
-                        <section className="message-status">Delivered</section>
-                    </section>
-                    <section className="message send">
-                        <section className="message-content">hello, it is me and I’m typing and it is working as expected and what do you think</section>
-                        <section className="message-status">Delivered</section>
-                    </section>
-                </section>
-                {/* send message box */}
-                <form action="" className="send-message">
-                    <textarea onKeyUp={(e) => {
-                        e.target.style.height = '31px'
-                        let height = e.target.scrollHeight
-                        height += 1
-                        e.target.style.height = `${height}px`
-                    }} className="message-box" name="" id="" ></textarea>
-                    <button>
-                        <SendIcon />
-                    </button>
-                </form>
-            </section>
-
         </section>
     )
 }
