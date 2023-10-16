b90330aa49867606143ea6914c64493cdac2bb49
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-10-16 14:24:14 +0000

    checked the width of the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
 import './styles/fonts.css'
 import './styles/main.css'
 import Register from './components/Register'
@@ -9,19 +8,7 @@ import Login from './components/Login'
 import Home from './components/Home'
 import ForgotPassword from './components/ForgotPassword'
 import VerifyEmail from './components/VerifyEmail'
-
 import NewFriends from './components/NewFriends'
-
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// const queryClient = new QueryClient({
-//     defaultOptions: {
-//         queries: {
-//             staleTime: 1000,
-//             cacheTime: 1000
-//         }
-//     }
-// })
 const App = () => {
     return (
         <BrowserRouter>
@@ -31,8 +18,6 @@ const App = () => {
                 <Route path='/' element={<Home />} />
                 <Route path='/forgot-password' element={<ForgotPassword />} />
                 <Route path='/verify' element={<VerifyEmail />} />
-                {/* <Route path='/messages' element={<MessagePanel />} />
-                <Route path='/chats' element={<ChatPanel />} /> */}
                 <Route path='/new-friends' element={<NewFriends/>}/>
             </Routes>
         </BrowserRouter>
