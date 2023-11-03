fbf5570fbd5e71b5289cfda5ef36ef0de5e26375
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-11-03 16:09:10 +0000

    refactoring ui code base

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,19 +1,20 @@
 import { Link, useNavigate } from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faChevronDown, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import '../styles/style.css'
-import SearchIcon from './react-svg/SearchIcon'
-import ArrowDownIcon from './react-svg/ArrowDownIcon'
+
 import Dp from '../images/user-dp.png'
 const Chats = () => {
     const navigate = useNavigate()
     const handleChatClick = (e) => {
         e.preventDefault()
-        navigate('/messages')
+        // navigate('/messages')
     }
     const chats = []
     for (let i = 0; i < 20; i++) {
         chats.push(
-            <Link key={i} className='link' onClick={handleChatClick}>
-                <section className="chat">
+           
+                <Link className="chat">
                     <section className="picture-frame">
                         <img src={Dp} alt="user dp" />
                     </section>
@@ -22,26 +23,27 @@ const Chats = () => {
                             <section className="username">Lovis Tawiah</section>
                             <section className="last-message-time">11:30 pm</section>
                         </section>
+
                         <section className="last-message">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ipsum esse eius voluptatibus consequatur, ipsam quibusdam et eligendi corrupti doloremque eos, sint nulla temporibus nam facilis alias veritatis sed incidunt?
                         </section>
                     </section>
-                </section>
-            </Link>
+                </Link>
         )
     }
     return (
         <section className="chats-panel">
-            <div className="search-box">
+            <div className="search-container">
                 <section className="search-holder">
-                    <SearchIcon />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
                     <input type="text" name="" placeholder='Search' id="" />
                 </section>
             </div>
             <section className="chats-text-container">
                 <section className="chats-text">Chats</section>
-                <ArrowDownIcon />
+              <FontAwesomeIcon icon={faChevronDown} />
             </section>
+
             <section className="chats">
                 {chats}
             </section>
