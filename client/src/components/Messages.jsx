fd31a13078302251084583976a745853f1c512a9
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-11-04 16:37:17 +0000

    fixing messages panel

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -2,13 +2,15 @@ import { Textarea } from '@chakra-ui/react'
 import SendIcon from './react-svg/SendIcon'
 import Dp from '../images/user-dp.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDown, faArrowLeft, faArrowRight, faChevronLeft, faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft  } from '@fortawesome/free-solid-svg-icons'
 const Messages = ({ panel }) => {
     const messages = []
     for (let i = 0; i < 20; i++) {
-        messages.push(<section className="message">
-            <section className="message-content">hello, it is me and I’m typing and it is working as expected and what do you think</section>
+        messages.push(
+        <section className="message">
+            <p className="message-content">hello, it is me and I’m typing and it is working as expected and what do you think
             <section className="message-status">Delivered</section>
+            </p>
         </section>)
     }
     return (
@@ -19,10 +21,12 @@ const Messages = ({ panel }) => {
                 <section className="chat-dp">
                     <img src={Dp} alt="user db" />
                 </section>
+
                 <section className="chat-username-status">
                     <section className="chat-username">Lovis Tawiah</section>
                     <section className="chat-status">Online</section>
                 </section>
+
             </section>
 
             <section className="messages">
