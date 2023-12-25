6942a0e56b311e2c46b07547d0bc5106f9760c5a
lovistawiah <lovistawiah638@gmail.com>
2023-12-25 14:13:04 +0000

    fixed messages ui issues:

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,17 +3,32 @@ import { messageHeaderDate, messageStatus } from "../utils/compareDate"
 const Message = ({ message, sender, createdAt, userId }) => {
     const formattedMessageHeaderDate = messageHeaderDate(createdAt)
     const messageStatusText = messageStatus(createdAt)
+    let itemsClass, messageStatusAlign, textColor, messageContainerBg = ""
+    if (sender === userId) {
+        itemsClass = 'self-end'
+        messageStatusAlign = 'self-start'
+        textColor = 'text-zinc-100'
+        messageContainerBg = 'bg-blue-600'
 
-    const itemsClass = `items-${sender !== userId ? 'end' : 'start'}`
-    const textColor = `text-${sender === userId ? 'zinc-300' : 'neutral-100'}`
-    const messageContainerBg = `bg-${sender !== userId ? 'blue-600' : 'zinc-600'}`
-    console.log(messageContainerBg)
+    } else {
+        itemsClass = 'self-start'
+        messageStatusAlign = 'self-end'
+        textColor = 'text-zinc-600'
+        messageContainerBg = 'bg-neutral-300'
+    }
+
     return (
         <>
             <section className="w-min h-auto p-1 bg-white rounded-full border border-stone-500  items-center flex self-center text-center text-black text-sm font-normal">{formattedMessageHeaderDate}</section>
-            <section className={`min-w-min flex flex-col ${itemsClass}`} >
-                <section className={`rounded[25px] max-w-[255px] ${textColor} ${messageContainerBg}`}>{message}</section>
-                <section className="text-center text-zinc-800 text-sm font-normal">{messageStatusText}</section>
+
+            <section className={`min-w-min flex flex-col ${itemsClass} mx-2 my-2`} >
+                <section className={`${textColor} rounded-[25px] max-w-[255px] p-[8px] ${messageContainerBg}`}>
+                    {message}
+                </section>
+
+                <section className={`${messageStatusAlign} text-center text-zinc-800 text-sm font-normal`}>
+                    {messageStatusText}
+                </section>
             </section>
         </>
 
