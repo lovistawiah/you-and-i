4c51393466d9815fc1de2e8fc9c264d51f6b3850
lovistawiah <lovistawiah638@gmail.com>
2024-01-25 20:27:27 +0000

    uncommented Search Component in Chats and deleted react-svg folder

--- src/components/react-svg/ArrowDownIcon.jsx
+++
@@ -1,8 +0,0 @@
-const ArrowDownIcon = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 16">
-            <path d="M2.585 0.822266L11 9.79143L19.415 0.822266L22 3.58352L11 15.3335L0 3.58352L2.585 0.822266Z"  />
-        </svg>
-    )
-}
-export default ArrowDownIcon
