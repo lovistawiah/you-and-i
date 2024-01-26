60089cc4a707e021e4e28f1d73faeaa663141c77
lovistawiah <lovistawiah638@gmail.com>
2024-01-26 11:00:38 +0000

    fixed ui issues in MessageHeaderDate.jsx

--- src/components/MessageHeaderDate.jsx
+++ src/components/MessageHeaderDate.jsx
@@ -1,6 +1,6 @@
 const MessageHeaderDate = ({ messageDate }) => {
     return <>
-        <section className="w-min h-auto p-1 bg-white rounded-full border border-stone-100  items-center flex self-center text-center text-black text-[12px] font-normal my-1">{messageDate}</section>
+        <section className="h-auto p-1 bg-white rounded-full border border-stone-100  items-center flex self-center text-center text-black text-[12px] font-normal my-1">{messageDate}</section>
 
     </>
 };
