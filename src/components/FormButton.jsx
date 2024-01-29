546ccd68927a04289217be01cb5b389e3b994a94
lovistawiah <lovistawiah638@gmail.com>
2024-01-29 19:13:58 +0000

    fixed button width issue

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,6 +1,6 @@
 const FormButton = ({ btnText }) => {
     return (
-        <button className='w-[280px] h-[33px] px-3.5 py-[7px] font-rale  bg-blue-600 rounded-[5px] border border-neutral-500 flex items-center justify-center text-white text-base font-normal hover:bg-blue-700 active:bg-blue-800 outline-none  md:w-[300px] md:text-lg '
+        <button className='w-[200px] h-[33px] px-3.5 py-[7px] font-rale  bg-blue-600 rounded-[5px] border border-neutral-500 flex items-center justify-center text-white text-base font-normal hover:bg-blue-700 active:bg-blue-800 outline-none  md:w-[300px] md:text-lg '
         >{btnText}
         </button>
     )
