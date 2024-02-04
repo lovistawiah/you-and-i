882f980eb64d55b35a563c5a5960da2762f634f7
lovistawiah <lovistawiah638@gmail.com>
2024-02-04 11:00:30 +0000

    added event name, clear chatInfo then add

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -26,6 +26,7 @@ const Chat = ({ chatId, userId, username, avatarUrl, lastMessage, lstMsgDate }) 
 
         }
         dispatch(clearMessages())
+        dispatch(setChatInfo({}))
         dispatch(setChatInfo(chatObj))
     }
 
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -19,15 +19,16 @@ const Contacts = () => {
     const clearSearch = () => {
         setSearchInput('');
     };
-    const handleUserInfo = ({ userId, channelId, avatarUrl, username }) => {
+    const handleUserInfo = ({ userId, chatId, avatarUrl, username }) => {
         const chatObj = {
             userId,
-            channelId,
+            chatId,
             avatarUrl,
             username,
             status
         }
         dispatch(clearMessages())
+        dispatch(setChatInfo({}))
         dispatch(setChatInfo(chatObj))
 
     }
@@ -76,7 +77,7 @@ const Contacts = () => {
             <section className='overflow-y-auto mt-[129px] absolute top-2 bottom-[56px] left-0 right-0 w-full md:bottom-1'>
                 {
                     cachedContacts?.map((contact) => (
-                        <Link to={`/${windowWidth < 768 ? 'messages' : ''}`} onClick={() => handleUserInfo({ userId: contact._id, username: contact.username, avatarUrl: contact.avatarUrl, status: contact.status })} className="w-full justify-start items-center flex" key={contact._id} >
+                        <Link to={`/${windowWidth < 768 ? 'messages' : ''}`} onClick={() => handleUserInfo({ userId: contact._id, username: contact.username, avatarUrl: contact.avatarUrl, status: contact.status, chatId: contact?.chatId })} className="w-full justify-start items-center flex" key={contact._id} >
                             <section className="w-[70px] h-[65px] p-2.5 justify-center items-center flex shrink-0">
                                 <img src={contact.avatarUrl} alt="user profile" className='rounded-full' />
                             </section>
@@ -85,7 +86,7 @@ const Contacts = () => {
                                     {contact.username}
                                 </h4>
                                 <section className="text-neutral-400 text-sm font-normal line-clamp-2 text-ellipsis w-full flex-grow basis-0 pt-[4px] pr-0 pb-[40px] pl-1">
-                                    bio
+                                    {contact.bio}
                                 </section>
                             </section>
                         </Link >
