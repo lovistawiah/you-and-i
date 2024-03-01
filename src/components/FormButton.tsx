2967ee0d24e2dfb49d039e464de598fee25023a1
lovistawiah <lovistawiah638@gmail.com>
2024-03-01 10:58:41 +0000

    type checked FormButton.tsx

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -1,6 +1,15 @@
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const FormButton = ({ btnText, spin, isValid }) => {
+
+const FormButton = ({
+  btnText,
+  spin,
+  isValid,
+}: {
+  btnText: string;
+  spin: boolean;
+  isValid: boolean;
+}) => {
   return (
     <button
       className={`h-[33px] w-[200px] px-3.5 py-[7px] font-roboto ${!isValid ? "bg-red-500" : " bg-blue-500"} flex items-center justify-center rounded-[5px] border text-base text-white font-normal${isValid ? "bg-red-500" : "hover:bg-blue-600"} ${!isValid ? "bg-red-700" : " active:bg-blue-700"} outline-none  disabled:cursor-not-allowed md:w-[300px] md:text-lg`}
