5f1641e0b6f07f4d4e8ec90091c6da408518677c
lovistawiah <lovistawiah638@gmail.com>
2024-03-09 20:56:59 +0000

    fix module path error

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUserInfo } from "../app/userSlice";
-import { UserInfo } from "../interface/account/user";
+import { UserInfo } from "../account/user";
+
 
 const useRegister = () => {
   const navigate = useNavigate();
