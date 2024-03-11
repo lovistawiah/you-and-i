967f744bd73e9c10da86a988751c08c222171303
lovistawiah <lovistawiah638@gmail.com>
2024-03-11 17:49:46 +0000

    add modal for info message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
 import { store } from "./app/store";
 import Login from "./components/Login";
 import MainPage from "./components/MainPage";
@@ -11,20 +10,18 @@ import Register from "./components/Register";
 import "/index.css";
 import MessagePanel from "./components/MessagePanel";
 
-const AnimatedPage = () => {
+const RoutePages = () => {
   const location = useLocation();
 
   return (
-    <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/update-profile" element={<UpdateProfile />} />
-        <Route path="/messages" element={<MessagePanel />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </AnimatePresence>
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/update-profile" element={<UpdateProfile />} />
+      <Route path="/messages" element={<MessagePanel />} />
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 };
 
@@ -33,7 +30,7 @@ const App = () => {
     <div className="m-0 box-border w-screen bg-gray-50 p-0 font-roboto">
       <BrowserRouter>
         <Provider store={store}>
-          <AnimatedPage />
+          <RoutePages />
         </Provider>
       </BrowserRouter>
     </div>
--- src/components/InfoContainer.tsx
+++ src/components/InfoContainer.tsx
@@ -1,28 +1,34 @@
-import { useState } from "react";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const InfoContainer = ({
-  info,
-  setInfo,
-}: {
-  info:
-    | Record<string, never>
-    | {
-        type: string;
-        message: string;
-      };
-  setInfo: React.Dispatch<
-    React.SetStateAction<
-      | Record<string, never>
-      | {
-          type: string;
-          message: string;
-        }
-    >
-  >;
-}) => {
-  return <div></div>;
+const InfoContainer = () => {
+  const elRef = useRef<HTMLDivElement | null>(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+  useEffect(() => {
+    if (!elRef.current) return;
+    const modalRoot = document.getElementById("modal") as HTMLDivElement;
+    modalRoot.appendChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+  return createPortal(
+    <div className="absolute left-2 right-2 top-0 rounded border">
+      <div className="h-[50px] w-full bg-red-500 ">
+        <FontAwesomeIcon
+          icon={faTimes}
+          className="md:text-[16px absolute right-2 top-2 cursor-pointer rounded border p-2 font-roboto text-[18px] font-light text-white transition duration-300 ease-in-out hover:bg-white hover:text-red-500"
+        />
+      </div>
+    </div>,
+    elRef.current,
+  );
 };
 
 export default InfoContainer;
