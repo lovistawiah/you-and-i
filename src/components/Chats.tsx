2d4e477755d1762dd01b0336734b37f3f8bb4ac2
lovistawiah <lovistawiah638@gmail.com>
2024-03-22 17:08:21 +0000

    Import Search.tsx in Chats.tsx:

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,8 +1,7 @@
 import PageHeader from "./PageHeader";
 import Chat from "./Chat";
 import useChats from "../hooks/useChats";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+import Search from "./Search";
 
 const ChatList = () => {
   const { searchInput, handleSearch, clearSearch, chats } = useChats();
@@ -10,31 +9,7 @@ const ChatList = () => {
   return (
     <section className={`relative  order-2 w-full md:w-[55%] md:border-r`}>
       <PageHeader pageName={"Chats"} />
-
-      <div className="fixed top-[59px] flex h-[70px] w-full flex-col items-center justify-center bg-gray-50 px-2.5 md:relative md:top-[10px]">
-        <section className="relative w-[90%]">
-          {!searchInput && (
-            <FontAwesomeIcon icon={faSearch} className="absolute left-1 top-[10px] text-zinc-500" />
-          )}
-          <input
-            type="text"
-            name="search"
-            placeholder="Search"
-            id="search"
-            className="inline-flex h-9 w-full items-center justify-start gap-1.5 border-b-[1px] border-zinc-500 bg-white pl-6 text-base font-normal outline-none"
-            value={searchInput}
-            onChange={handleSearch}
-          />
-          {searchInput && (
-            <FontAwesomeIcon
-              icon={faTimes}
-              className="absolute right-3 top-[10px] cursor-pointer text-zinc-500"
-              onClick={clearSearch}
-            />
-          )}
-        </section>
-      </div>
-
+      <Search clearSearch={clearSearch} handleSearch={handleSearch} searchInput={searchInput} />
       <div className="absolute bottom-[56px] left-0 right-0 top-2 mt-[129px] w-full overflow-y-auto md:bottom-1">
         {Array.isArray(chats) && chats.length > 0 ? (
           chats.map((chat) => (
